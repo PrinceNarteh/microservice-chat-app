67e07b826bdbd05ffbf3db6784d570f331dda195
Prince Narteh <prinarteh@gmail.com>
2025-12-08 03:47:50 +0000

    feat: minor changes

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -12,12 +12,12 @@ export const createLogger = (options: CreateLoggerOptions): Logger => {
   const transport =
     process.env.NODE_ENV === 'development'
       ? {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-          },
-        }
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+        },
+      }
       : undefined;
 
   return pino({
@@ -26,4 +26,4 @@ export const createLogger = (options: CreateLoggerOptions): Logger => {
     transport,
     ...rest,
   });
-;
+};
